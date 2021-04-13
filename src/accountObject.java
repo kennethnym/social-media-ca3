e6513320551bae882fdb30abd94e73b648abb3e9
christophergayiuman <christophergayiuman@gmail.com>
2021-04-13 23:56:18 +0800

    accountObject basics

---
+++ src/accountObject.java
@@ -0,0 +1,14 @@
+public class accountObject {
+
+    private int id;
+    private String handle;
+    private String description;
+
+    public accountObject(int id, String handle, String description){
+        this.id = id;
+        this.handle = handle;
+        this.description = description;
+    }
+
+
+}
