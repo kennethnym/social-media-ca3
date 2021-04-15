fe5b7949d55b7feb38966a1600c939cac238c64e
christophergayiuman <christophergayiuman@gmail.com>
2021-04-16 01:37:09 +0800

    update main testing file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import socialmedia.SocialMediaPlatformImpl;
 import socialmedia.exceptions.AccountIDNotRecognisedException;
+import socialmedia.exceptions.HandleNotRecognisedException;
 import socialmedia.exceptions.IllegalHandleException;
 import socialmedia.exceptions.InvalidHandleException;
 import socialmedia.models.Account;
@@ -14,14 +15,29 @@ public class Main {
 
         //Create a new account
         try {
-            socialMediaPlatform.createAccount("Banan", "I am banana");
+            socialMediaPlatform.createAccount("ban", "I am banana");
+            System.out.println(socialMediaPlatform.showArraylists());
+
+            socialMediaPlatform.createAccount("tooty", "I am tooty");
+            System.out.println(socialMediaPlatform.showArraylists());
+
+            socialMediaPlatform.createAccount("jam", "I am jam");
+            System.out.println(socialMediaPlatform.showArraylists());
+
+            System.out.print("total: ");
+            System.out.println(socialMediaPlatform.showArraylists());
+
+
+            System.out.println(socialMediaPlatform.showAccount("jam"));
+
 
         } catch (InvalidHandleException e) {
             e.printStackTrace();
         } catch (IllegalHandleException e) {
             e.printStackTrace();
+        } catch (HandleNotRecognisedException e) {
+            e.printStackTrace();
         }
-
 
 
     }
